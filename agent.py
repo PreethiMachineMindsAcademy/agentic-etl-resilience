e1c44f48bed5f449aae36b04b9109ad5886027c7
PreethiMachineMindsAcademy <preethileelaselvaraj@outlook.com>
2026-02-19 16:55:57 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,1 +1,9 @@
+def restart_pipeline(p):
+    return f"Restart triggered for {p}"
+
+def fix_schema(p):
+    return f"Schema sync for {p}"
+
+def scale_resources(p):
+    return f"Scaling resources for {p}"
 
