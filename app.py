df91bbcdd370a967553fcd8be251f147994fe981
PreethiMachineMindsAcademy <preethileelaselvaraj@outlook.com>
2026-02-19 16:56:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+import streamlit as st
+import json
+from agent import agentic_loop
+
+st.title("🤖 Agentic AI for Autonomous ETL Resilience")
+
+with open("logs.json") as f:
+    logs=json.load(f)
+
+for log in logs:
+    st.subheader(log["pipeline"])
+    st.write("Error:",log["error"])
+
+    if st.button(f"Analyze {log['pipeline']}"):
+        decision,action=agentic_loop(log)
+        st.success("Decision: "+decision)
+        st.info("Action: "+action)
 
